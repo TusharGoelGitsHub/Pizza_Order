2f4e959b0a6a66c51c5fd84a21663e4809ba9e97
Tushar Goel <tushargoel15@gmail.com>
2024-03-29 21:50:13 +0530

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PizzaOrderForm from "./components/PizzaOrderForm/PizzaOrderForm";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import OrdersDetailsPage from "./components/OrdersDetailsPage/OrdersDetailsPage";
+import { useEffect, useState } from "react";
+import { IPizzaOrder } from "./components/PizzaOrderForm/PizzaOrderForm.types";
 
 function App() {
+  const [orderData, setOrderData] = useState<IPizzaOrder[]>([]);
+  const [orderIdCounter, setOrderIdCounter] = useState<number>(1);
+  useEffect(() => {
+    console.log(orderData);
+  }, [orderData]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PizzaOrderForm
+                orderData={orderData}
+                setOrderData={setOrderData}
+                orderIdCounter={orderIdCounter}
+                setOrderIdCounter={setOrderIdCounter}
+              />
+            }
+          />
+          <Route
+            path="/detailsSection"
+            element={
+              <OrdersDetailsPage
+                orderData={orderData}
+                setOrderData={setOrderData}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
